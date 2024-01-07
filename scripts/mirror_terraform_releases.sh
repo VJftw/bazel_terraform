d7742c2b0ee2176e0d220ed569cced07c189dde6
VJ Patel <vj@vjpatel.me>
2024-01-07 02:52:18 +0000

    Add GHA workflow to update versions

--- scripts/mirror_terraform_releases.sh
+++ scripts/mirror_terraform_releases.sh
@@ -3,7 +3,17 @@
 # replace the `./terraform/private/versions.bzl` file.
 set -Eeuo pipefail
 
+# https://www.hashicorp.com/.well-known/pgp-key.txt
+hashicorp_pgp_key_file="$1"
+
 HASHICORP_RELEASES_BASE_URL="https://releases.hashicorp.com"
+# From: https://www.hashicorp.com/trust/security
+KEY_ID="72D7468F"
+
+export GNUPGHOME="$(mktemp -d)"
+chmod 0700 "$GNUPGHOME"
+gpg --import "$hashicorp_pgp_key_file"
+echo -e "5\nquit\n" | gpg --command-fd 0 --expert --edit-key $KEY_ID trust
 
 mapfile -t terraform_release_url_dirs < \
     <(
@@ -22,10 +32,16 @@ for terraform_release_url in "${terraform_release_url_dirs[@]}"; do
     json="$(echo "$json" | jq --arg version "$version" '.[$version] = {}')"
 
     sha256sums_url="${terraform_release_url}terraform_${version}_SHA256SUMS"
-    mapfile -t sha256sum_lines < \
-        <(
-            curl -sL "${sha256sums_url}"
-        )
+    sha256sums_sig_url="${terraform_release_url}terraform_${version}_SHA256SUMS.sig"
+    >&2 curl -sL "${sha256sums_url}" -o "terraform_${version}_SHA256SUMS"
+    >&2 curl -sL "${sha256sums_sig_url}" -o "terraform_${version}_SHA256SUMS.sig"
+    if ! gpg --verify "terraform_${version}_SHA256SUMS.sig" "terraform_${version}_SHA256SUMS" 2>&1 | grep "Good signature" > /dev/null; then
+        echo "could not find good signature for terraform_${version}_SHA256SUMS.sig"
+        exit 1
+    fi
+
+    mapfile -t sha256sum_lines < "terraform_${version}_SHA256SUMS"
+
     for sha256sum_line in "${sha256sum_lines[@]}"; do
         sha256sum="$(echo "$sha256sum_line" | cut -f1 -d" ")"
         platform="$(echo "$sha256sum_line" | awk '{ print $2 }' | rev | cut -f1-2 -d_ | rev | cut -f1 -d.)"
